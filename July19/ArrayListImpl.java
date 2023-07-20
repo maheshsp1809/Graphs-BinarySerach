e03aaf9526214ee9ab6bf71213556bce41d09414
maheshsp <maheshsp1809@gmail.com>
2023-07-20 16:49:15 +0530

    started implementing graph

---
+++ July19/ArrayListImpl.java
@@ -0,0 +1,49 @@
+package July19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayListImpl {
+    public static void main(String[] args) {
+        try (Scanner scanobj = new Scanner(System.in)) {
+            System.out.println("Enter number of vertices: ");
+            int V = scanobj.nextInt();
+            System.out.println("Enter number of edges:");
+            int E = scanobj.nextInt();
+
+            List<List<Integer>> edges = new ArrayList<>(E);
+
+            System.out.println("Enter the edges of the graph:");
+            for (int i = 0; i < E; i++) {
+                int u = scanobj.nextInt();
+                int v = scanobj.nextInt();
+                // Adding the edge to the list
+                List<Integer> edge = Arrays.asList(u, v);
+                edges.add(edge);
+            }
+            List<List<Integer>> ansList = adjaList(V, E, edges);
+            for (List<Integer> row : ansList) {
+                System.out.println(row);
+            }
+        }
+
+    }
+
+    public static List<List<Integer>> adjaList(int V, int E, List<List<Integer>> edges) {
+        List<List<Integer>> AdjList = new ArrayList<List<Integer>>();
+        for (int i = 0; i < V; ++i) {
+            AdjList.add(new ArrayList<>());
+        }
+        for (List<Integer> edge : edges) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            AdjList.get(edge.get(u)).add(edge.get(v));
+            AdjList.get(edge.get(v)).add(edge.get(u));
+        }
+        return AdjList;
+
+    }
+
+}
---
+++ July19/HashmapImpl.java
@@ -0,0 +1,62 @@
+package July19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashmapImpl {
+    public static void main(String[] args) {
+        Scanner scanobj = new Scanner(System.in);
+        System.out.println("Enter number of vertices: ");
+        int V = scanobj.nextInt();
+        System.out.println("Enter number of edges:");
+        int E = scanobj.nextInt();
+
+        List<List<Integer>> edges = new ArrayList<>(E);
+
+        System.out.println("Enter the edges of the graph:");
+        for (int i = 0; i < E; i++) {
+            int u = scanobj.nextInt();
+            int v = scanobj.nextInt();
+            // Adding the edge to the list
+            List<Integer> edge = Arrays.asList(u, v);
+            edges.add(edge);
+        }
+        Map<Integer, List<Integer>> adjMap = adjMapFun(V, E, edges);
+
+        // Print adjMap
+        for(adjMap.keySet().iterator().hasNext()){
+            
+        }
+        for (Map.Entry<Integer, List<Integer>> entry : adjMap.entrySet()) {
+            int vertex = entry.getKey();
+            List<Integer> neighbors = entry.getValue();
+
+            System.out.print("Vertex " + vertex + " -> ");
+            for (int neighbor : neighbors) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static Map<Integer, List<Integer>> adjMapFun(int V, int E, List<List<Integer>> edges) {
+        Map<Integer, List<Integer>> adjMap = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < V; ++i) {
+            adjMap.put(i, new ArrayList<Integer>());
+
+        }
+        for (List<Integer> edge : edges) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            adjMap.get(u).add(v);
+            adjMap.get(v).add(u);
+        }
+        return adjMap;
+
+    }
+}
