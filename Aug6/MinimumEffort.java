719cfa22552f1e71b590febc7d09507963a394f6
maheshsp <maheshsp1809@gmail.com>
2023-08-06 22:32:58 +0530

    added again

--- Aug6/MinimumEffort.java
+++ Aug6/MinimumEffort.java
@@ -2,43 +2,73 @@ package Aug6;
 
 import java.util.*;
 
-class Edge {
-    int u;
-    int v;
-    int weight;
+import java.util.*;
 
-    public Edge(int u, int v, int weight) {
-        this.u = u;
-        this.v = v;
-        this.weight = weight;
+class Tuple {
+    int distance;
+    int row;
+    int col;
+
+    public Tuple(int distance, int row, int col) {
+        this.row = row;
+        this.distance = distance;
+        this.col = col;
     }
 }
 
 class Solution {
-    public List<Edge> primMST(List<List<Edge>> graph, int startVertex) {
-        List<Edge> mst = new ArrayList<>();
-        Set<Integer> visited = new HashSet<>();
-        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
+    public int minimumEffortPath(int[][] heights) {
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((x, y) -> x.distance - y.distance);
+        int n = heights.length;
+        int m = heights[0].length;
 
-        visited.add(startVertex);
-        pq.addAll(graph.get(startVertex));
-
-        while (!pq.isEmpty() && visited.size() < graph.size()) {
-            Edge edge = pq.poll();
-            int u = edge.u;
-            int v = edge.v;
-
-            if (visited.contains(u) && !visited.contains(v)) {
-                mst.add(edge);
-                visited.add(v);
-                pq.addAll(graph.get(v));
-            } else if (visited.contains(v) && !visited.contains(u)) {
-                mst.add(edge);
-                visited.add(u);
-                pq.addAll(graph.get(u));
+        int[][] dist = new int[n][m];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                dist[i][j] = (int) (1e9);
             }
         }
+        dist[0][0] = 0;
+        pq.add(new Tuple(0, 0, 0));
+        int[] dr = { -1, 0, 1, 0 };
+        int[] dc = { 0, 1, 0, -1 };
 
-        return mst;
+        while (!pq.isEmpty()) {
+            Tuple it = pq.poll();
+            int diff = it.distance;
+            int row = it.row;
+            int col = it.col;
+
+            if (row == n - 1 && col == m - 1)
+                return diff;
+
+            for (int i = 0; i < 4; ++i) {
+                int newr = row + dr[i];
+                int newc = col + dc[i];
+
+                if (newr >= 0 && newc >= 0 && newr < n && newc < m) {
+                    int newEffort = Math.max(Math.abs(heights[row][col] - heights[newr][newc]), diff);
+
+                    if (newEffort < dist[newr][newc]) {
+                        dist[newr][newc] = newEffort;
+                        pq.add(new Tuple(newEffort, newr, newc));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] heights = {
+                { 1, 2, 2 },
+                { 3, 8, 2 },
+                { 5, 3, 5 }
+        };
+
+        int minEffort = solution.minimumEffortPath(heights);
+        System.out.println("Minimum Effort Path: " + minEffort);
     }
 }
