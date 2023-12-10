c74820df3c46bb97f1deb5d2faadfa74fcf5f001
maheshsp <maheshsp1809@gmail.com>
2023-12-10 16:14:18 +0530

    BS with lower bound

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public static int search(int[] nums, int t) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == t) {
+                return mid;
+            } else if (nums[mid] > t) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 7;
+
+        int result = search(sortedArray, target);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index: " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+    }
+}
+
