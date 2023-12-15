7a0a3fc7cd3a48c96bc657460da973edbc0342da
maheshsp <maheshsp1809@gmail.com>
2023-12-15 18:33:11 +0530

    Dynamic cost

---
+++ Nov29/Nov29/MinimumDistance.java
@@ -0,0 +1,37 @@
+package Nov29;
+
+import java.util.Arrays;
+
+public class MinimumDistance {
+
+    public static int[] minDistances(int N, int[] locations) {
+        int[] result = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int[] distances = new int[N];
+            for (int j = 0; j < N; j++) {
+                distances[j] = Math.abs(locations[j] - locations[i]);
+            }
+
+            Arrays.sort(distances);
+
+            int totalDistance = 0;
+            for (int j = 0; j < N - 2; j++) {
+                totalDistance += distances[j];
+            }
+
+            result[i] = totalDistance;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int N = 3;
+        int[] locations = { 1, 3, 8 };
+
+        int[] result = minDistances(N, locations);
+
+        System.out.println("Output: " + Arrays.toString(result));
+    }
+}
---
+++ twomax.java
@@ -0,0 +1,5 @@
+package Graphs-BinarySerach;
+
+public class twomax {
+
+}
